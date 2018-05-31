eb3d881afc73037940aa0fa9168939b21c66f22e
badmusamuda <badmusamuda@gmail.com>
2018-05-31 16:19:49 +0100

    Pangram added

--- src/main/java/com/herokuapp/uwrite/exercism/levelone/Pangram.java
+++ src/main/java/com/herokuapp/uwrite/exercism/levelone/Pangram.java
@@ -1,4 +1,26 @@
 package com.herokuapp.uwrite.exercism.levelone;
 
+import java.util.Random;
+
 public class Pangram {
+
+    public static String isASCII(final String INPUT){
+
+        String response = "NOT";
+
+        if( INPUT.matches("[0-9]{1,}"))
+            response = "NOT";
+
+        return "This is "+response+" a pangram sentence";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+
+                isASCII("hello world klepow ")
+
+        );
+
+    }
 }
